fac3b137ac1492cd8cb4c9b3b924b5ee0e26351f
Eitan Revach <eitan@revachonline.net>
2018-02-02 16:38:26 +0200

    Add Express to project
    - first app with server listening
    - simple config file (port)

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('First time');
+"use strict";
+
+var express = require('express');
+var config = require('./config/default.json');
+
+//creatd express app
+var app = express();
+
+// Routers
+
+// Test
+app.get('/Test', function (req, res) {
+  res.send('Hello World');
+});
+
+// Error Handling
+app.use(function (err, req, res, next) {
+  console.log(JSON.stringify(err));
+  console.log("EXCEPTION :: " + err.message);
+  console.log(err.stack);
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  });
+})
+
+console.log("Start listening on port " + config.port);
+app.listen(config.port);
